334db0b7d4bfd5402c954560abb8c846e60a00b9
Yaronberg <Yaronmen123@gmail.com>
2020-03-09 15:41:13 +0200

    fix bugs

--- Pagesaver/Pagesaver/Program.cs
+++ Pagesaver/Pagesaver/Program.cs
@@ -9,39 +9,65 @@ namespace Pagesaver
     {
         static void Main(string[] args)
         {
-
-            Console.WriteLine("Введите запрос типа: pagesaver selin.in.ua/index.html output.html");
-            var request = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
-
-            var serverAddr = request[1].Split("/")[0];
-            //var a = $@"D:\";
-            var Path = request[2];
-            Console.WriteLine("tcp client");
-
-            var message = $"GET /index.html HTTP/1.0\n\n";
-
-            try
+            while (true)
             {
-                var port = 80;
 
-                var client = new TcpClient(serverAddr, port);
-                var data = System.Text.Encoding.ASCII.GetBytes(message);
-                NetworkStream stream = client.GetStream();
 
-                stream.Write(data, 0, data.Length);
-                stream.Flush();
-                Console.WriteLine("Sent {0}", message);
+                Console.WriteLine("Введите запрос типа:\n" +
+                    "pagesaver selin.in.ua output.html\n" +
+                    "pagesaver ua.fm output.html\n" +
+                    "pagesaver http://selin.in.ua/solvve/text.txt output.html\n" +
+                    "pagesaver http://selin.in.ua/solvve/html.html output.html\n" +
+                    "pagesaver http://info.cern.ch/hypertext/WWW/TheProject.html output.html");
 
-                var responseData = new byte[65536];
-                int bytesRead = stream.Read(responseData, 0, responseData.Length);
-                var responseMessage = System.Text.Encoding.ASCII.GetString(responseData, 0, bytesRead);
-                Console.WriteLine("Receive {0}\n", responseMessage);
-                stream.Close();
-                client.Close();
+                var request = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+                string addr = request[1];
+                string serverAddr = addr;
+
+                if (addr.IndexOf("http://") >= 0)
+                {
+                    addr = addr.Substring(7);
+
+                }
+
+                serverAddr = addr.Split("/")[0];
+
+                if (addr.IndexOf("/") != -1)
+                {
+                    addr = addr.Substring(addr.IndexOf("/"));
+                }
+                else
+                {
+                    addr = "/";
+                }
+
+                var Path = "..\\..\\..\\" + request[2];
+
+                var message = $"GET {addr} HTTP/1.0\r\nHost: {serverAddr}\r\n\r\n";
+                Console.WriteLine("before:" + message);
 
                 try
                 {
-                    responseMessage = responseMessage.Substring(responseMessage.IndexOf("\n\n"));
+                    var port = 80;
+
+                    var client = new TcpClient(serverAddr, port);
+                    var data = System.Text.Encoding.ASCII.GetBytes(message);
+                    NetworkStream stream = client.GetStream();
+
+                    stream.Write(data, 0, data.Length);
+                    stream.Flush();
+                    Console.WriteLine("Sent {0}", message);
+
+                    var responseData = new byte[65535];
+                    int bytesRead = stream.Read(responseData, 0, responseData.Length);
+                    var responseMessage = System.Text.Encoding.ASCII.GetString(responseData, 0, bytesRead);
+                    Console.WriteLine("Receive {0}\n", responseMessage);
+                    stream.Close();
+                    client.Close();
+
+                    var indexString = responseMessage.IndexOf("<html>") == -1 ? responseMessage.IndexOf("\r\n") : responseMessage.IndexOf("<html>");
+                    responseMessage = responseMessage.Substring(indexString);
 
                     using (StreamWriter sw = new StreamWriter(Path, false, System.Text.Encoding.Default))
                     {
@@ -49,19 +75,15 @@ namespace Pagesaver
                     }
 
                     Console.WriteLine("Запись выполнена");
+
                 }
                 catch (Exception e)
                 {
-
-                    Console.WriteLine(e);
+                    Console.WriteLine("Exception: " + e);
                 }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-            }
 
-            Console.ReadLine();
+                Console.WriteLine();
+            }
         }
     }
 }
