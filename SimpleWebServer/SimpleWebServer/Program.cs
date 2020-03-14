ed75ae63391eb3eee3e9ef8e9216db4741056688
Yaronberg <Yaronmen123@gmail.com>
2020-03-15 00:35:53 +0200

    add code

--- SimpleWebServer/SimpleWebServer/Program.cs
+++ SimpleWebServer/SimpleWebServer/Program.cs
@@ -1,12 +1,49 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
 
-namespace SimpleWebServer
+namespace serverSolvve
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("simple server");
+            try
+            {
+                var port = 19333;
+                var localAddr = IPAddress.Parse("127.0.0.1");
+                var server = new TcpListener(localAddr, port);
+                server.Start();
+                var bytes = new byte[8192];
+
+                while (true)
+                {
+                    Console.WriteLine("wating for connection");
+                    var client = server.AcceptTcpClient();
+
+                    NetworkStream stream = client.GetStream();
+
+                    string content = "<h1>Hi, everyone!</h1>";
+
+                    string header =
+                        "HTTP/1.0 200 OK\r\n" +
+                        "Server: SimpleServer\r\n" +
+                        "Content-Type: text/html\r\n" +
+                        "Content-Length: " + content.Length + "\r\n\r\n";
+
+                    var response = System.Text.Encoding.ASCII.GetBytes(header + content);
+                    stream.Write(response, 0, response.Length);
+
+                    Console.WriteLine("sent: {0}\n", header);
+
+                }
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
         }
     }
 }
